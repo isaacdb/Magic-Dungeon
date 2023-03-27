89b18be0c9ed902bb67b6722deb7f1eb222f870d
isaacdb <isaacdbs@outlook.com>
2023-03-27 11:06:04 -0300

    add spawning enemies room system

--- Scripts/Room.gd
+++ Scripts/Room.gd
@@ -1,17 +1,31 @@
 extends Node2D
 
-@onready var areaRoom := $Area2D as Area2D
 @export var gateEnter : Gate
+@export var gateExit : Gate
+@export var enemies : Array[PackedScene]
+
+@onready var areaRoom := $Area2D as Area2D
+@onready var spawnPoints = self.get_node("SpawnPoints").get_children() as Array[Node2D]
+@onready var rand = RandomNumberGenerator.new()
 
 var areaClean = false
 
 func _ready():
+	rand.randomize()
 	areaRoom.connect("body_entered", area_room_body_entered)
 	pass # Replace with function body.
-
 
 func area_room_body_entered(body):
 	if !areaClean and body.is_in_group("player"):
 		areaClean = true
 		gateEnter.close()
+		spawn_enemies()
+	pass
+	
+func spawn_enemies():
+	for enemy in enemies:
+		var newEnemy = enemy.instantiate() as CharacterBody2D
+		get_tree().root.call_deferred("add_child", newEnemy)
+		var positionIndex = rand.randi_range(0, spawnPoints.size() - 1)		
+		newEnemy.global_position = spawnPoints[positionIndex].global_position
 	pass
