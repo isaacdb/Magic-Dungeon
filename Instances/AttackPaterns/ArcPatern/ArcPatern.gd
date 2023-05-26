b69c6d23df5b3bba94c07b2d124aaf46a44061d2
isaacdb <isaacdbs@outlook.com>
2023-05-26 13:04:05 -0300

    create new arc attack patern

---
+++ Instances/AttackPaterns/ArcPatern/ArcPatern.gd
@@ -0,0 +1,47 @@
+extends Node2D
+class_name ArcPatern
+
+@onready var shooter := $ShooterComponent as ShooterComponent
+@onready var playerTrack := $PlayerTracker as PlayerTracker
+@onready var rnd := RandomNumberGenerator.new()
+
+@export var fireAmount : int = 5
+@export var anglePerFire : float = 2.0
+@export var delayBetweenFire : float = 0.1
+@export var bullet : BulletStats
+
+var angleFire : float = 0.0
+var firstBulletFired : bool = false
+
+var direction : Vector2
+
+func _ready():
+	rnd.randomize()
+	pass
+	
+func _physics_process(delta):
+	
+	if Input.is_action_just_pressed("dash"):
+		Execute()
+	pass
+
+func Execute():
+	direction = playerTrack.GetDirection()
+	angleFire = 0.0
+	firstBulletFired = false
+	
+	var tween = create_tween().set_loops(fireAmount)
+	tween.tween_callback(Fire).set_delay(delayBetweenFire)
+	tween.play()
+	pass
+	
+func Fire():
+	if !firstBulletFired:
+		shooter.JustFire(direction, bullet)
+		firstBulletFired = true
+	else:
+		angleFire += anglePerFire
+		shooter.JustFire(Vector2.RIGHT.rotated(direction.angle() + deg_to_rad(angleFire)), bullet)
+		shooter.JustFire(Vector2.RIGHT.rotated(direction.angle() - deg_to_rad(angleFire)), bullet)
+		
+	pass
