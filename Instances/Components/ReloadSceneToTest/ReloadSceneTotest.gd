35d1d4e7188a603fe7f2f0404f767ca75aa8c4bb
isaacdb <isaacdbs@outlook.com>
2023-09-22 12:07:44 -0300

    build and cheats adjustment

--- Instances/Components/ReloadSceneToTest/ReloadSceneTotest.gd
+++ Instances/Components/ReloadSceneToTest/ReloadSceneTotest.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+@export var player : Mage1
+
+@export var bossRommTp : Marker2D
+
 func _physics_process(delta):	
 	if Input.is_action_pressed("ui_text_backspace"):
-		Global.level_up.emit()
+		player.global_position = bossRommTp.global_position
 	
-#
-#	if Input.is_action_just_pressed("dash"):
-#		Global.level_up.emit()a
+	if Input.is_action_just_pressed("ui_end"):
+		Global.level_up.emit()
 
