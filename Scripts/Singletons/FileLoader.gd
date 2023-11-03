a6c262f6b740062f91995b0e84171c38867e321d
isaacdb <isaacdbs@outlook.com>
2023-11-03 18:41:02 -0300

    fix load resource at web build

--- Scripts/Singletons/FileLoader.gd
+++ Scripts/Singletons/FileLoader.gd
@@ -7,6 +7,11 @@ func get_resouces_by_folder(path: String) -> Array[Resource]:
 		dir.list_dir_begin()
 		var file_name = dir.get_next();
 		while file_name != "":
+			
+			## Fix bug to load resoucers at Web builds
+			if '.tres.remap' in file_name:
+				file_name = file_name.trim_suffix('.remap')
+			
 			if dir.current_is_dir():
 				listOfResources.append_array(get_resouces_by_folder(path + file_name + "/"));
 			else:
